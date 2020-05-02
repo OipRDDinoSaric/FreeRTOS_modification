962a543870b17c9461f41db1c77abe2a75ecc611
dinosaric <dinsaric@gmail.com>
2020-05-02 20:29:14 +0200

    initial

---
+++ user_app_template/Inc/user_app_template.h
@@ -0,0 +1,8 @@
+/**
+ * @file custom_bootloader.c
+ *
+ * @brief   Custom bootloader for STM32F4 Disc1 development board with
+ *          STM32F407. Uses UART for communication
+ */
+
+/****END OF FILE****/
---
+++ user_app_template/Src/user_app_template.c
@@ -0,0 +1,15 @@
+/**
+ * @file custom_bootloader.c
+ *
+ * @brief   Custom bootloader for STM32F4 Disc1 development board with
+ *          STM32F407. Uses UART for communication
+ *
+ * @note    Written according to BARR-C:2018 coding standard
+ *          Exceptions:
+ *              - 3.2 a, c  - Eclipse formater doesn't support
+ *              - 6.3 b iv. - ERR_CHECK() macro has return keyword
+ *              - 7.1 m     - Boolean begins with is, e.g. is_example
+ */
+#include "user_app_template.h"
+
+/****END OF FILE****/
