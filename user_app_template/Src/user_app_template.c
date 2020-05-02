14c06ac4232ae11b27cf4278b4a6488a2755f7f6
dinosaric <dinsaric@gmail.com>
2020-05-02 20:35:05 +0200

    changed name in comment

--- user_app_template/Src/user_app_template.c
+++ user_app_template/Src/user_app_template.c
@@ -1,5 +1,5 @@
 /**
- * @file custom_bootloader.c
+ * @file user_app_template.c
  *
  * @brief   Custom bootloader for STM32F4 Disc1 development board with
  *          STM32F407. Uses UART for communication
