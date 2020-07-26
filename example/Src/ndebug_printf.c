e7d6da7053f973f79149a98ed7b2e8f83a6722c6
dinosaric <dinsaric@gmail.com>
2020-07-26 22:35:53 +0200

    Add mutex around printf to make it atomic

--- example/Src/ndebug_printf.c
+++ example/Src/ndebug_printf.c
@@ -5,16 +5,24 @@
  *
  */
 
-#include <stdio.h>
-#include <stdarg.h>
-
-#include <usart.h>
-
 #include "ndebug_printf.h"
 
 #ifndef NDEBUG
 
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdbool.h>
+
+#include <usart.h>
+
+#include <FreeRTOS.h>
+#include <semphr.h>
+
 #define PRINTF_UART_HANDLE huart2
+
+static bool isInited = false;
+
+static SemaphoreHandle_t mutex_freertos_atomic = NULL;
 
 #ifdef __GNUC__
 int __io_putchar(int ch)
@@ -26,13 +34,30 @@ int fputc(int ch, FILE *f)
   return ch;
 }
 
+
 int	ndebug_printf(const char *format, ...)
 {
 	va_list args;
-	va_start(args, format);
-	int retval = vprintf(format, args);
-	va_end(args);
-	return retval;
+
+	if(!isInited)
+	{
+	    isInited = true;
+	    mutex_freertos_atomic = xSemaphoreCreateMutex();
+        xSemaphoreGive(mutex_freertos_atomic);
+	}
+
+	if(xSemaphoreTake(mutex_freertos_atomic, pdMS_TO_TICKS(2 * 1000)) == pdTRUE)
+	{
+        va_start(args, format);
+        int retval = vprintf(format, args);
+        va_end(args);
+        xSemaphoreGive(mutex_freertos_atomic);
+        return retval;
+	}
+	else
+	{
+	    return -1;
+	}
 }
 
 #else
